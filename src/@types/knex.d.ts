54de477ba39857fa578d6b22fd58168507471566
souzannatha <souzanatha04@gmail.com>
2025-07-30 19:55:05 -0300

    feat: add types in tables

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    users: {
+      id: string
+      name: string
+      created_at: string
+    }
+  }
+}
